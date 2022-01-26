01f2c73ddb59e8a32241a4b4668ff8f033e348ab
Mateus Garcia <mateus.gigainfo@gmail.com>
2022-01-26 18:41:38 -0300

    chore(update)

--- src/extensions/typeorm.extension.ts
+++ src/extensions/typeorm.extension.ts
@@ -2,6 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbHost, dbHostPort, dbUser, dbPassword, dbSchema } from '../shared/configs/db.conf';
 
 
+
 export const typeORMConnection = TypeOrmModule.forRoot( {
   type: 'postgres',
   host: dbHost,
@@ -17,3 +18,40 @@ export const typeORMConnection = TypeOrmModule.forRoot( {
     connectionLimit: 50
   }
 } );
+
+
+// opções para bancos com replicaset
+
+// export const typeORMWriterConnection = TypeOrmModule.forRoot( {
+//   name: 'default',
+//   type: 'mysql',
+//   host: process.env.MYSQL_W_HOST_NAME,
+//   port: +process.env.MYSQL_W_HOST_PORT,
+//   username: process.env.MYSQL_W_USER,
+//   password: process.env.MYSQL_W_PASSWORD,
+//   database: 'myDB',
+//   entities: [ __dirname + '/../**/entities/transactional/*.entity{.ts,.js}' ],
+//   keepConnectionAlive: false,
+//   synchronize: false,
+//   logging: process.env.NODE_ENV === 'test' || 'test:dev' ? false : true,
+//   extra: {
+//     connectionLimit: 50
+//   }
+// } );
+
+// export const typeORMReaderConnection = TypeOrmModule.forRoot( {
+//   name: 'reader',
+//   type: 'mysql',
+//   host: process.env.MYSQL_R_HOST_NAME,
+//   port: +process.env.MYSQL_R_HOST_PORT,
+//   username: process.env.MYSQL_R_USER,
+//   password: process.env.MYSQL_R_PASSWORD,
+//   database: 'myReplicaDB',
+//   entities: [ __dirname + '/../**/entities/views/*.entity{.ts,.js}' ],
+//   keepConnectionAlive: false,
+//   synchronize: false,
+//   logging: process.env.NODE_ENV === 'test' || 'test:dev' ? false : true,
+//   extra: {
+//     connectionLimit: 50
+//   }
+// } );
